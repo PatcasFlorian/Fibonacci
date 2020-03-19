1e3bacb93607197e987ff3ad06121c22ecc2f263
Patcas Florian <patcasf12@gmail.com>
2020-03-19 17:26:14 +0200

    ConsoleHelpers

---
+++ SirFibonacci/SirFibonacci/ConsoleHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SirFibonacci
+{
+    public static class ConsoleHelper
+    {
+        public static int ReadNumber(string message,int maxAttemps,int defaultValue)
+        {
+            int attemptsCount = 1;
+            bool isNumber = false;
+            while(!isNumber)
+            {
+                Console.Write(message);
+                string text = Console.ReadLine();
+                isNumber = int.TryParse(text, out int number);
+
+                if (isNumber)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine($"'{text}' os not a nuber , tray again ({maxAttemps-attemptsCount} attempts remaning)...");
+                }
+
+                attemptsCount++;
+                if (attemptsCount > maxAttemps)
+                {
+                    Console.WriteLine($" Max attempts countexceeded , asuming {defaultValue} as default value");
+                    return defaultValue;
+                }
+            }
+            return defaultValue;
+
+        }
+       
+    }
+}
